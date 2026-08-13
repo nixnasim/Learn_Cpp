a4351eac4fd9f4577b4a45b9cbe422e1e4a0e6b1
Nasimul Islam Nasim <nasimulislamnasim6@gmail.com>
2026-08-13 18:21:39 +0600

    new

---
+++ Class_44_Introduction_to_array.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+int marks [5] = {80,70,60,50,40,};
+
+for (int i=0; i<=4; i++)
+{
+cout<< marks[i]<<"    " ;
+}
+
+// int marks [5];
+
+    // marks [0] = 80;
+    // marks [1] = 70;
+    // marks [2] = 60;
+    // marks [3] = 50;
+    // marks [4] = 40;
+
+// cout<< marks [0] <<"    ";
+// cout<< marks [1] <<"    ";
+// cout<< marks [2] <<"    ";
+// cout<< marks [3] <<"    ";
+// cout<< marks [4] <<"    ";
+
+return 0;
+}
