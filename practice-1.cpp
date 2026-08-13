4d571f819e0b1c24540f7541a44763afde3b98b4
Nasimul Islam Nasim <nasimulislamnasim6@gmail.com>
2026-08-13 10:36:06 +0600

    new

---
+++ practice-1.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main ()
+{
+int i = 1;
+while (i <=500)
+{
+    cout<<i <<"Eastern University" <<endl;
+    i++;
+}
+cout<< "End of the while loop";
+
+return 0;    
+}
